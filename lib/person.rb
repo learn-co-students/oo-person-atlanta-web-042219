273f2e9d2f24817fb8bafd624dcf851a753b3298
Will Jones <willjones2010@gmail.com>
2019-04-24 16:22:12 -0400

    almost there

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,32 @@
 # your code goes here
+class Person
+attr_accessor :bank_account
+
+  def initialize (name)
+    @name = name
+    @balance = 25
+    @happiness = 8
+  end
+
+  def name
+    @name
+  end
+
+  def bank_account
+    @balance
+  end
+
+  def happiness
+    @happiness
+  end
+
+  def happiness= (points)
+    if points < 10
+      @happiness = points
+    elsif points < 0
+      @happiness = 0
+    
+    end
+  end
+
+end
